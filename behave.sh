63a4b77b0713f849cfb16a7d2c88749e4c811725
Praveen Kumar <114041720+praveen1895@users.noreply.github.com>
2022-12-26 12:24:12 +0530

    Update behave.sh

--- behave.sh
+++ behave.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-echo "steps to run BDD"
-sudo mkdir -p /home/ubuntu/features/steps
-sudo cp -r ./test.feature /home/ubuntu/features
-sudo cp -r ./test.py /home/ubuntu/features/steps
+echo "installing behave"
+pip_install()
+{
+        sudo apt-get update
+        sudo apt install python3-pip -y
+        if [ $? -ne 0 ]; then
+                echo "error while installing pip"
+        fi
+        echo "pip installed successfully"
+}
+behave_install()
+{
+        pip install behave
+        if [ $? -ne 0 ]; then
+                echo "error while installing behave"
+        fi
+        echo "behave installed successfully"
+}
+behave_steps()
+{
+        echo "steps to run BDD"
+        sudo mkdir -p /home/ubuntu/features/steps
+        sudo cp -r ./test.feature /home/ubuntu/features
+        sudo cp -r ./test.py /home/ubuntu/features/steps
+        behave
+}
+pip_install
+behave_install
+behave_steps
