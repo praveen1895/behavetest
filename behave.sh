58f769c68534d7a055264c2919b16cf9409ade25
Praveen Kumar <114041720+praveen1895@users.noreply.github.com>
2022-12-26 12:20:49 +0530

    Update behave.sh

--- behave.sh
+++ behave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "steps to run BDD"
 sudo mkdir -p /home/ubuntu/features/steps
-sudo cp -r .
-sudo cp -r ./test.py /home/ubuntu/feat/steps
+sudo cp -r ./test.feature /home/ubuntu/features
+sudo cp -r ./test.py /home/ubuntu/features/steps
